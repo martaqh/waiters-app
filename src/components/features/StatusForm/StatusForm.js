76d29358a25e383f172b213b260cd221434e50b8
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-13 19:04:06 +0200

    Fix status values options

--- src/components/features/StatusForm/StatusForm.js
+++ src/components/features/StatusForm/StatusForm.js
@@ -1,24 +1,20 @@
-import { useSelector } from "react-redux";
 import styles from './StatusForm.module.scss';
-import { getAllStatusValues } from "../../../redux/tablesRedux";
 import FormSectionTitle from "../../common/FormSectionTitle/FormSectionTitle";
 import shortid from "shortid";
 
 const StatusForm = props => {
-    
-    const allStatusValues = useSelector(state => getAllStatusValues(state));
-    const uniqueStatusValues = [...new Set(allStatusValues)];
-    const statusValues = uniqueStatusValues.filter(status => status !== props.status);
+    const statusValues = ['busy', 'free', 'reserved', 'cleaning'];
+    const statusOptions = statusValues.filter(status => status !== props.currentStatus);
     
     return (
         <div className={styles.statusForm}>
             <FormSectionTitle>Status:</FormSectionTitle>
             <select className={styles.select + " form-select"}
                     aria-label="Default select example"
-                    value={props.status}
+                    value={props.currentStatus}
                     onChange={props.onChange}>
-                <option selected>{props.status}</option>
-                {statusValues.map(status => <option key={shortid()}>{status}</option> )}
+                <option>{props.currentStatus}</option>
+                {statusOptions.map(status => <option key={shortid()}>{status}</option> )}
             </select>
         </div>
         
