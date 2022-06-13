f888ee8e8bfae9ac3df3f0471aef15a12bc53d26
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-13 16:19:41 +0200

    Update StatusForm component

--- src/components/features/StatusForm/StatusForm.js
+++ src/components/features/StatusForm/StatusForm.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import styles from './StatusForm.module.scss';
 import { getAllStatusValues } from "../../../redux/tablesRedux";
+import FormSectionTitle from "../../common/FormSectionTitle/FormSectionTitle";
 
 const StatusForm = props => {
     
@@ -9,10 +10,14 @@ const StatusForm = props => {
     const statusValues = uniqueStatusValues.filter(status => status !== props.status);
     
     return (
-        <select className={styles.select + " form-select"} aria-label="Default select example">
-            <option selected>{props.status}</option>
-            {statusValues.map(status => <option>{status}</option> )}
-        </select>
+        <div className={styles.statusForm}>
+            <FormSectionTitle>Status:</FormSectionTitle>
+            <select className={styles.select + " form-select"} aria-label="Default select example">
+                <option selected>{props.status}</option>
+                {statusValues.map(status => <option>{status}</option> )}
+            </select>
+        </div>
+        
     )
 }
 
