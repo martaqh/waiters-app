68d1db5407c7af984381d82a2a18e4469e69408d
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-13 16:20:54 +0200

    Update TablePage using form components

--- src/components/pages/TablePage/TablePage.js
+++ src/components/pages/TablePage/TablePage.js
@@ -1,25 +1,45 @@
 import { useSelector } from "react-redux";
 import { useParams } from 'react-router-dom';
+import { useState } from "react";
 import { getTableById } from "../../../redux/tablesRedux";
+import PeopleNumberForm from "../../features/PeopleNumberForm/PeopleNumberForm";
 import StatusForm from "../../features/StatusForm/StatusForm";
+import BillForm from "../../features/BillForm/BillForm";
+import Button from '../../common/Button/Button';
 
 const TablePage = () => {
     const { id } = useParams();
     const table = useSelector(state => getTableById(state, id));
+
+    const [status, setStatus] = useState(table.status);
+    const [people, setPeople] = useState(table.people);
+    const [places, setPlaces] = useState(table.places);
+    const [bill, setBill] = useState(table.bill);
+
     if (table.status === 'busy')
     return ( 
         <main>
             <h2>Table {table.id}</h2>
-            <StatusForm status={table.status}>{table.status}</StatusForm> 
-            <p>People: {table.people} / {table.places}</p>
-            <p>Bill: {table.bill}</p>
+            <StatusForm 
+                status={status}
+                onChange={e => setStatus(e.target.value)} />
+            <PeopleNumberForm
+                people={people}
+                places={places}
+                onChangePeople={e => setPeople(e.target.value)}
+                onChangePlaces={e => setPlaces(e.taget.value)}  />
+            <BillForm
+                bill={bill}
+                onChange={e => setBill(e.target.value)} />
+            <Button>Update</Button>
         </main>
     )
     return (
         <main>
             <h2>Table {table.id}</h2>
             <StatusForm status={table.status}>{table.status}</StatusForm> 
-            <p>People: {table.people} / {table.places}</p>
+            <PeopleNumberForm people={table.people} places={table.places} />
+            <Button>Update</Button>
         </main>
     )
 
